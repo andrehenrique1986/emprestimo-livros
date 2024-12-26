19f766885de2de7326996a73e0717d0eb681fa6a
andrehenrique1986 <andrehenriquelima1986@gmail.com>
2024-12-26 11:50:56 -0300

    Envio de Requisições REST via Postman

--- EmprestimoLivros.API/Controllers/ClienteController.cs
+++ EmprestimoLivros.API/Controllers/ClienteController.cs
@@ -8,7 +8,7 @@ namespace EmprestimoLivros.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ClienteController : ControllerBase
+    public class ClienteController : Controller
     {
 
         private readonly IClienteRepository _clienteRepository;
@@ -18,12 +18,27 @@ namespace EmprestimoLivros.API.Controllers
             _clienteRepository = clienteRepository;
         }
 
-        [HttpGet]
+        [HttpGet("selecionarTodos")]
         public async Task<ActionResult<IEnumerable<Cliente>>> GetClientes()
         {
             var clientes = await _clienteRepository.SelecionarTodos();
             return Ok(clientes);
         }
 
+
+        [HttpPost("cadastrarCliente")]
+        public async Task<ActionResult> CadastrarCliente(Cliente cliente)
+        {
+             _clienteRepository.Incluir(cliente);
+
+            if (await _clienteRepository.SaveAllAsync())
+            {
+                return Ok("Cliente cadastrado com sucesso !");
+            }
+
+            return BadRequest("Ocorreu um erro ao salvar o cliente");
+        }
+
     }
 }
+
